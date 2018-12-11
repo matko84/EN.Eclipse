6bf718795da9faf9a8d05387c39f990246deab77
matko84 <>
2018-12-11 23:29:14 +0100

    Edunova03

---
+++ Edunova03/src/edunova/Zadatak5.java
@@ -0,0 +1,13 @@
+package edunova;
+
+public class Zadatak5 {
+	
+	//Program učitava 4 broja.
+	//u slučaju da je prvi jednak zadnjem broju ispisuje
+	//zbroj drugog i trećeg broja
+	//inače ispisuje sljedno brojeve od najmanjeg prema najvećem
+	
+	
+	// DOVRŠITI ZA DZ
+
+}
